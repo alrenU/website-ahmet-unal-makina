c19fe7cf9536d86c5f1037ee4f49c0b62794a4ea
Ali Eren <alierenunal957@gmail.com>
2023-12-02 13:50:08 +0300

    Make header responsive, add quick-contact info to the website.

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,49 @@
 // HAMBURGER MENU
+let openHamburgerMenu = false;
+
+const lineOne = document.getElementById("hamburger-line-one");
+const lineTwo = document.getElementById("hamburger-line-two");
+const lineThree = document.getElementById("hamburger-line-three");
+const headerNav = document.getElementById("header-nav");
+
+function handleHamburgerMenuClick() {
+    if (!openHamburgerMenu) {
+        Object.assign(lineOne.style, {
+            transform: "translate(0px, 9px) rotate(45deg)"
+        });
+
+        Object.assign(lineTwo.style, {
+            opacity: "0",
+            transform: "translate(-10px, 0px)"
+        });
+
+        Object.assign(lineThree.style, {
+            transform: "translate(0px, -9px) rotate(-45deg)"
+        });
+
+        Object.assign(headerNav.style, {
+            display: "block",
+            right: "0px"
+        });
+    } else {
+        Object.assign(lineOne.style, {
+            transform: "translate(0px, 0px) rotate(0deg)"
+        });
+
+        Object.assign(lineTwo.style, {
+            opacity: "1",
+            transform: "translate(0px, 0px)"
+        });
+
+        Object.assign(lineThree.style, {
+            transform: "translate(0px, 0px) rotate(0deg)"
+        });
+
+        Object.assign(headerNav.style, {
+            display: "none",
+            right: "-50px"
+        });
+    }
+
+    openHamburgerMenu = !openHamburgerMenu;
+}
