67574b647db65ffd1d9d3f63e71e2e8d17b37368
Kadir Engin <kadirengin2000@hotmail.com>
2023-12-03 19:54:34 +0300

    add static product page.

---
+++ assets/js/readmore.js
@@ -0,0 +1,13 @@
+// we need to add node.js
+
+// function handleLongText() {
+//     let textP = document.getElementById("detailsText");
+//     let text = textP.innerHTML
+
+    
+//     if(text.length > 150){
+//         textP.innerHTML = text.slice(0, 150) + "..."
+//     }
+// }
+
+// handleLongText()
