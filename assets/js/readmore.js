9e08bf7480d0149c7ddb0845c4e3d52d3145a5cc
Kadir Engin <kadirengin2000@hotmail.com>
2023-12-04 22:46:19 +0300

    add read more.

--- assets/js/readmore.js
+++ assets/js/readmore.js
@@ -1,13 +1,27 @@
-// we need to add node.js
-
-// function handleLongText() {
-//     let textP = document.getElementById("detailsText");
-//     let text = textP.innerHTML
+let textP = document.getElementById("detailsText");
+let text = textP.innerHTML;
+let readMore = document.createElement("p");
 
     
-//     if(text.length > 150){
-//         textP.innerHTML = text.slice(0, 150) + "..."
-//     }
-// }
+    if(text.length > 150){
+        shrinkText();
+        readMore.className = "read-more";
+        readMore.onclick = () => handleLongText();
+        document.getElementById("text-container").appendChild(readMore);
+    }
 
-// handleLongText()
+function handleLongText() {
+    if(readMore.innerHTML === "Read More!") expandText();
+    else shrinkText();
+}
+
+function expandText() {
+    textP.innerHTML = text;
+    readMore.innerHTML = "Read Less!";
+}
+
+function shrinkText() {
+    textP.innerHTML = text.slice(0, 150) + "...";
+    readMore.innerHTML = "Read More!";
+}
+
