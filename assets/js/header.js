18ef32a57a73b678eefe20635f770ddc36479266
Ali Eren <alierenunal957@gmail.com>
2023-12-06 13:25:18 +0300

    Update header.

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,17 @@
 // Shrink the header height after the page is being scrolled.
-window.addEventListener("scroll", function (event) {
-    const scrollPositionY = window.scrollY;
-    const headerElement = this.document.getElementsByClassName("header")[0];
+// window.addEventListener("scroll", function (event) {
+//     const scrollPositionY = window.scrollY;
+//     const headerElement = this.document.getElementsByClassName("header")[0];
 
-    if(scrollPositionY < 150) {
-        if(scrollPositionY < 100) {
-            headerElement.style.height = "100px";
-        }
-    } else {
-        headerElement.style.height = "80px";
-    }
-    console.log("scrollPositionY: ", scrollPositionY);
-});
+//     if(scrollPositionY < 150) {
+//         if(scrollPositionY < 100) {
+//             headerElement.style.height = "100px";
+//         }
+//     } else {
+//         headerElement.style.height = "80px";
+//     }
+//     console.log("scrollPositionY: ", scrollPositionY);
+// });
 
 // HAMBURGER MENU
 let openHamburgerMenu = false;
@@ -19,12 +19,12 @@ let openHamburgerMenu = false;
 const lineOne = document.getElementById("hamburger-line-one");
 const lineTwo = document.getElementById("hamburger-line-two");
 const lineThree = document.getElementById("hamburger-line-three");
-const headerNav = document.getElementById("header-nav");
+const headerNav = document.getElementById("navigation");
 
 function handleHamburgerMenuClick() {
     if (!openHamburgerMenu) {
         Object.assign(lineOne.style, {
-            transform: "translate(0px, 9px) rotate(45deg)"
+            transform: "translate(0px, 8px) rotate(45deg)"
         });
 
         Object.assign(lineTwo.style, {
@@ -33,7 +33,7 @@ function handleHamburgerMenuClick() {
         });
 
         Object.assign(lineThree.style, {
-            transform: "translate(0px, -9px) rotate(-45deg)"
+            transform: "translate(0px, -8px) rotate(-45deg)"
         });
 
         Object.assign(headerNav.style, {
